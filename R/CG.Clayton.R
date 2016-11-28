7c01523c6d47ea6e974c6113561817f126ab7fee
Takeshi Emura <takeshiemura@gmail.com>
2016-11-28 12:04:35 +0000

    version 3.0

---
+++ R/CG.Clayton.R
@@ -0,0 +1,18 @@
+CG.Clayton <-
+function(t.vec,d.vec,alpha,S.plot=TRUE,S.col="black"){
+  n=length(t.vec)
+  R=n:1
+  t.sort=sort(t.vec)
+  d.sort=d.vec[order(t.vec)]
+  A=( (R-1)/n )^(-alpha)-(R/n)^(-alpha)
+  A[n]=0
+  S=( 1+cumsum(A*d.sort) )^(-1/alpha)
+  
+  if(S.plot==TRUE){
+    plot(c(0,t.sort),c(1,S),type="s",lwd=3,
+         ylim=c(0.2,1),ylab="Survival probability",xlab="Time",col=S.col)
+    points(t.sort[d.sort==0],S[d.sort==0],pch=3,cex=2,col=S.col)
+  }
+  
+  list( time=t.sort,surv=S )
+}
